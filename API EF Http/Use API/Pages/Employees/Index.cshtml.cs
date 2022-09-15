2e4aca16ca47705bc2e305fefc3d4779e7d9a0ef
ducndthe153148 <ducndthe153148@fpt.edu.vn>
2022-09-15 23:28:33 +0700

    Test Get Employee Using api

---
+++ API EF Http/Use API/Pages/Employees/Index.cshtml.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Net.Http.Headers;
+using System.Text.Json;
+using Use_API.Models;
+
+namespace Use_API.Pages.Employees
+{
+    public class IndexModel : PageModel
+    {
+        public IEnumerable<Employee> Employees { get; set; }
+        private PRN231DBContext _context = new PRN231DBContext();
+        private readonly HttpClient client = null;
+        private string EmployeeApiUrl = "";
+        public IndexModel()
+        {
+            client = new HttpClient();
+            var contentType = new MediaTypeWithQualityHeaderValue("application/json");
+            client.DefaultRequestHeaders.Accept.Add(contentType);
+            EmployeeApiUrl = "http://localhost:5000/api/employee/";
+        }
+        public async Task<IActionResult> OnGet()
+        {
+            HttpResponseMessage response = await client.GetAsync(EmployeeApiUrl);
+            string stringData = await response.Content.ReadAsStringAsync();
+            var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true};
+            Employees = JsonSerializer.Deserialize<List<Employee>>(stringData, options);
+            return Page();
+        }
+    }
+}
